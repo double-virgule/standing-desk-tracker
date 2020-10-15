d5203c6538d54341a33f123f5b9e1e895a60345b
Mark Mansur <16672129+thepuppet33r@users.noreply.github.com>
2020-10-15 14:19:38 -0500

    Created new version of standing desk tracker - 'SDT2.py'

--- SDT2.py
+++ SDT2.py
@@ -1,48 +1,78 @@
 import tkinter as Tkinter 
 from datetime import datetime 
 import time
-import csv 
+import pickle
+import csv
 
-global Sitting_Run
-global Standing_Run
-global FullStanding_Time
-global FullSitting_Time
-global StandingStart_Time
-global SittingStart_Time
-global StandingEnd_Time
-global SittingEnd_Time
+Standing_Run = False
+Sitting_Run = False
+FullStanding_Time = 0
+FullSitting_Time = 0
+StandingStart_Time = 0
+SittingStart_Time = 0
+StandingEnd_Time = 0 
+SittingEnd_Time = 0
 
-def Sitting(label):  
+def Sitting(label):
+    global Standing_Run
+    global Sitting_Run
+    global FullStanding_Time
+    global FullSitting_Time
+    global StandingStart_Time
+    global SittingStart_Time
+    global StandingEnd_Time
+    global SittingEnd_Time
+
+    print(Sitting_Run)
+    print(Standing_Run)
+
     if (Standing_Run == True):
         Standing_Run = False
         StandingEnd_Time = time.time()
         TemporaryMath = StandingEnd_Time - StandingStart_Time
         FullStanding_Time = TemporaryMath + FullStanding_Time
-    Sitting_Run == True
+    Sitting_Run = True
     SittingStart_Time = time.time()
-    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
+    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     label['text']=CurrentTimePretty
-    Sitting['state'] = 'Disabled'
-    Standing['state'] = 'Normal'
+    sitting['state']='disabled'
+    standing['state']='normal'
 
 def Standing(label):
+    global Standing_Run
+    global Sitting_Run
+    global FullStanding_Time
+    global FullSitting_Time
+    global StandingStart_Time
+    global SittingStart_Time
+    global StandingEnd_Time
+    global SittingEnd_Time
+
+    print(Sitting_Run)
+    print(Standing_Run)
+
     if (Sitting_Run == True):
         Sitting_Run = False
         SittingEnd_Time = time.time()
         TemporaryMath = SittingEnd_Time - SittingStart_Time
         FullSitting_Time = TemporaryMath + FullSitting_Time
-    Sitting_Run == True
-    SittingStart_Time = time.time()
-    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
+        print(FullSitting_Time)
+    Standing_Run = True
+    StandingStart_Time = time.time()
+    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     label['text']=CurrentTimePretty
-    Standing['state'] = 'Disabled'
-    Sitting['state'] = 'Normal'
+    standing['state']='disabled'
+    sitting['state']='normal'
 
 def ExitProgram():
-    #Write to CSV 
-	
-	#[Standing Time]    [Sitting Time]
-	#[FullStanding_Time][FullSitting_Time]
+
+    print(FullSitting_Time)
+    print(FullStanding_Time)
+
+    sample_mod=[['SittingTime', FullSitting_Time, 'StandingTime', FullStanding_Time]]
+    with open('standingsittingtime.txt', 'w') as f:
+        w = csv.writer(f, dialect = 'excel-tab')
+        w.writerows(sample_mod)
     root.destroy()
     exit
 
@@ -50,15 +80,15 @@ def ExitProgram():
 root = Tkinter.Tk() 
 root.title("Standing/Sitting Tracker") 
 root.minsize(width=250, height=70) 
-label = Tkinter.Label(root, text="Welcome!", fg="black", font="Verdana 30 bold") 
+label = Tkinter.Label(root, text="Welcome!", fg="black", font="Verdana 10 bold") 
 label.pack() 
 f = Tkinter.Frame(root) 
-start = Tkinter.Button(f, text='Standing', width=6, command=lambda:Standing(label)) 
-stop = Tkinter.Button(f, text='Sitting',width=6, command=lambda:Sitting(label)) 
+standing = Tkinter.Button(f, text='Standing', width=6, command=lambda:Standing(label)) 
+sitting = Tkinter.Button(f, text='Sitting',width=6, command=lambda:Sitting(label)) 
 exitbutton = Tkinter.Button(f, text='Exit',width=6, command=ExitProgram) 
 f.pack(anchor = 'center',pady=5) 
-start.pack(side="left") 
-stop.pack(side ="left") 
+standing.pack(side="left") 
+sitting.pack(side ="left") 
 exitbutton.pack(side="left") 
 root.mainloop() 
 
