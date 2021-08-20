047a87c94308ad22e38da147d30b11b988fa43de
thepuppet33r <16672129+thepuppet33r@users.noreply.github.com>
2021-08-20 17:00:33 -0400

    Rebuild of SDT2.py
    After learning a bit more about Python, I rewrote the standing desk tracker a bit. 
    
    Changes: 
    * Now dumps into a text file every time you click a button so that you don't lose everything if the program dies. 
    * Cleaner output - In hours : minutes : seconds.

--- SDT2.py
+++ SDT2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import tkinter as Tkinter 
+import tkinter as Tkinter # sudo apt-get install python3-tk 
 from datetime import datetime 
 import time
-import csv
+import os
+
+### Sets variables
 
 Standing_Run = False
 Sitting_Run = False
@@ -12,94 +14,110 @@ StandingStart_Time = 0
 SittingStart_Time = 0
 StandingEnd_Time = 0 
 SittingEnd_Time = 0
+StandingSittingTotals = ""
+
+###
+
+def time_convert(sec):
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+    formatted = "{0}:{1}:{2}".format(int(hours),int(mins),sec)
+    return formatted
+
 
 def Sitting(label):
-    global Standing_Run
-    global Sitting_Run
-    global FullStanding_Time
-    global FullSitting_Time
-    global StandingStart_Time
-    global SittingStart_Time
-    global StandingEnd_Time
-    global SittingEnd_Time
+    print("Sitting")
+    global FullStanding_Time, FullSitting_Time, StandingStart_Time, SittingStart_Time, StandingSittingTotals
 
-    print(Sitting_Run)
-    print(Standing_Run)
+    current_time = datetime.now().strftime("%H:%M:%S")
 
-    if (Standing_Run == True):
-        Standing_Run = False
+    SittingStart_Time = time.time() 
+    
+    if (FullStanding_Time != 0):
         StandingEnd_Time = time.time()
-        TemporaryMath = StandingEnd_Time - StandingStart_Time
-        FullStanding_Time = TemporaryMath + FullStanding_Time
-    Sitting_Run = True
-    SittingStart_Time = time.time()
-    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    label['text']=CurrentTimePretty
+        TempMath = StandingEnd_Time - StandingStart_Time 
+        FullStanding_Time = FullStanding_Time + TempMath
+
+    if (FullStanding_Time == 0): 
+        FullStanding_Time = 0.001
+
+    print("FullStanding_Time = " + str(FullStanding_Time))
+    print("FullSitting_Time = " + str(FullSitting_Time))
+
+    p_fullstandtime = time_convert(int(FullStanding_Time))
+    p_fullsittime = time_convert(int(FullSitting_Time))
+
+    StandingSittingTotals = "Total Sitting Time: " + str(p_fullsittime) + "\nTotal Standing Time: " + str(p_fullstandtime) + "\nStarted Standing at " + str(current_time)
+    label['text'] = StandingSittingTotals
+
+
+    file1 = open("standingsitting.txt", "w") 
+    file1.write(StandingSittingTotals)
+    file1.close()
+
     sitting['state']='disabled'
     standing['state']='normal'
 
 def Standing(label):
-    global Standing_Run
-    global Sitting_Run
-    global FullStanding_Time
-    global FullSitting_Time
-    global StandingStart_Time
-    global SittingStart_Time
-    global StandingEnd_Time
-    global SittingEnd_Time
+    print("Standing")
+    global FullStanding_Time, FullSitting_Time, StandingStart_Time, SittingStart_Time, StandingSittingTotals
 
-    print(Sitting_Run)
-    print(Standing_Run)
+    current_time = datetime.now().strftime("%H:%M:%S")
 
-    if (Sitting_Run == True):
-        Sitting_Run = False
+    StandingStart_Time = time.time() 
+
+    if (FullSitting_Time != 0):
         SittingEnd_Time = time.time()
-        TemporaryMath = SittingEnd_Time - SittingStart_Time
-        FullSitting_Time = TemporaryMath + FullSitting_Time
-        print(FullSitting_Time)
-    Standing_Run = True
-    StandingStart_Time = time.time()
-    CurrentTimePretty = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    label['text']=CurrentTimePretty
+        TempMath = SittingEnd_Time - SittingStart_Time 
+        FullSitting_Time = FullSitting_Time + TempMath
+
+
+    if (FullSitting_Time == 0): 
+        FullSitting_Time = 0.001
+
+    p_fullstandtime = time_convert(int(FullStanding_Time))
+    p_fullsittime = time_convert(int(FullSitting_Time))
+
+    StandingSittingTotals = "Total Sitting Time: " + str(p_fullsittime) + "\nTotal Standing Time: " + str(p_fullstandtime) + "\nStarted Sitting at " + str(current_time)
+
+    print("FullStanding_Time = " + str(FullStanding_Time))
+    print("FullSitting_Time = " + str(FullSitting_Time))
+
+    label['text'] = StandingSittingTotals 
+ 
     standing['state']='disabled'
     sitting['state']='normal'
 
-def ExitProgram():
+def ExitProgram(FullSitting_Time, FullStanding_Time):
+    
+    file1 = open("standingsitting.txt", "w") 
+    file1.write(StandingSittingTotals)
+    file1.close()
 
-    print(FullSitting_Time)
-    print(FullStanding_Time)
+    nicedate = datetime.now().strftime("%y_%m_%d")
 
-    sample_mod=[['SittingTime', FullSitting_Time, 'StandingTime', FullStanding_Time]]
-    with open('standingsittingtime.txt', 'w') as f:
-        w = csv.writer(f, dialect = 'excel-tab')
-        w.writerows(sample_mod)
+    os.rename(r'standingsitting.txt',r'standingsitting' + nicedate + ".txt")
     root.destroy()
     exit
 
 
 root = Tkinter.Tk() 
 root.title("Standing/Sitting Tracker") 
-root.minsize(width=250, height=70) 
-label = Tkinter.Label(root, text="Welcome!", fg="black", font="Verdana 10 bold") 
+root.minsize(width=350, height=75)
+label = Tkinter.Label(root, text="Test", font="Verdana 10 bold") 
 label.pack() 
 f = Tkinter.Frame(root) 
 standing = Tkinter.Button(f, text='Standing', width=6, command=lambda:Standing(label)) 
 sitting = Tkinter.Button(f, text='Sitting',width=6, command=lambda:Sitting(label)) 
-exitbutton = Tkinter.Button(f, text='Exit',width=6, command=ExitProgram) 
+exitbutton = Tkinter.Button(f, text='Exit',width=6, command=lambda:ExitProgram(FullSitting_Time,FullStanding_Time)) 
 f.pack(anchor = 'center',pady=5) 
-standing.pack(side="left") 
-sitting.pack(side ="left") 
-exitbutton.pack(side="left") 
+standing.pack(side="left", padx=0,pady=10) 
+sitting.pack(side ="left", padx=0,pady=10) 
+exitbutton.pack(side="left", padx=0,pady=10) 
 root.mainloop() 
 
 #####
-
-
-
-
-
-
-
-
 
 
